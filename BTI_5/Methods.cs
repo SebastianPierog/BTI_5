3409f9d7b6371b23af6ce55373abdfa3a29b688d
Pan Ktos <lambda19935@gmail.com>
2021-01-11 10:51:38 +0100

    Dodaj pliki projektów.

---
+++ BTI_5/Methods.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BTI_5
+{
+    public static class Methods
+    {
+        public static IEnumerable<int> ToIntList(this string data) => data.Replace(" ", string.Empty).Select(x => (int)char.GetNumericValue(x)).ToList();
+        public static string ListToString(this IEnumerable<int> data) => string.Join("", data);
+        public static IEnumerable<int> To64BitK(this IEnumerable<int> input) => Enumerable.Repeat(0, Math.Max(0, 64 - input.Count())).Concat(input);
+    }
+}
---
+++ BTI_5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace BTI_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DES des = new DES();
+            StreamReader textFromStream = new StreamReader(@"C:\Users\Sebastian\Desktop\text.txt"); 
+            StreamReader keyFromStream = new StreamReader(@"C:\Users\Sebastian\Desktop\key.txt");
+            List<int> text = textFromStream.ReadLine().ToIntList().ToList();
+            List<int> key = keyFromStream.ReadLine().ToIntList().ToList();
+            Console.WriteLine($"Text (64-bit): {text.ListToString()}");
+            Console.WriteLine($"Key (64-bit): {key.ListToString()}");
+            var encrypted = des.Encrypt(text, key);
+            Console.WriteLine($"Coded: {encrypted.ListToString()}");
+            Console.WriteLine($"Encoded: {des.Decrypt(encrypted, key).ListToString()}");
+            Console.ReadLine();
+        }
+    }
+}
